050d5e9fd55203b6b547536b93270057336042f7
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2024-08-14 13:37:11 -0300

    fix: responsive home

--- src/js/pages/home/script.js
+++ src/js/pages/home/script.js
@@ -1,3 +1,4 @@
+/*-----------Button promo-------*/
 document.addEventListener('DOMContentLoaded', () => {
     const closeButton = document.querySelector('.close-btn');
     const promo = document.querySelector('.promo');
@@ -10,36 +11,52 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+/*-----------Slider------*/
+
 document.addEventListener('DOMContentLoaded', function() {
     const prevButton = document.querySelector('.nav-link.prev');
     const nextButton = document.querySelector('.nav-link.next');
     const testimonialsContent = document.querySelector('.testimonials-content');
     const testimonialItems = document.querySelectorAll('.testimonial-item');
-    
-    let currentIndex = 0;
-    const itemsPerPage = 3; // Number of testimonials per page
-    const totalPages = Math.ceil(testimonialItems.length / itemsPerPage);
-    
-    function updateTestimonials() {
-      const offset = -currentIndex * (100 / itemsPerPage);
-      testimonialsContent.style.transform = `translateX(${offset}%)`;
-    }
-    
-    prevButton.addEventListener('click', function(e) {
-      e.preventDefault();
-      if (currentIndex > 0) {
-        currentIndex--;
-        updateTestimonials();
-      }
-    });
-    
-    nextButton.addEventListener('click', function(e) {
-      e.preventDefault();
-      if (currentIndex < totalPages - 1) {
-        currentIndex++;
-        updateTestimonials();
-      }
-    });
-  });
   
+    let currentIndex = 0;
+    const itemsPerPage = 3; // Número de itens visíveis por vez
+    const totalItems = testimonialItems.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+  
+    function updateTestimonials() {
+        const offset = -currentIndex * (100 / totalPages); // Deslocamento percentual
+        testimonialsContent.style.transform = `translateX(${offset}%)`;
+    }
+  
+    prevButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        if (currentIndex === 0) {
+            currentIndex = totalPages - 1;
+        } else {
+            currentIndex--;
+        }
+        updateTestimonials();
+    });
+  
+    nextButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        if (currentIndex === totalPages - 1) {
+            currentIndex = 0;
+        } else {
+            currentIndex++;
+        }
+        updateTestimonials();
+    });
+
+    // Ajusta o tamanho do container para acomodar todos os itens
+    function adjustContainerWidth() {
+        const containerWidth = totalItems * 100 / itemsPerPage;
+        testimonialsContent.style.width = `${containerWidth}%`;
+    }
+
+    adjustContainerWidth();
+});
+
+
 
